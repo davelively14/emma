2d66b4f8ac4eae4f96053cd4cabd3f5da8c8a23d
Dave Lively <davelively14@gmail.com>
2020-02-23 09:58:08 -0500

    Update config and add deps

--- mix.exs
+++ mix.exs
@@ -43,7 +43,9 @@ defmodule Emma.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:phoenix_live_view, "~> 0.7.1"}
+      {:phoenix_live_view, "~> 0.7.1"},
+      {:guardian, "~> 2.0"},
+      {:argon2_elixir, "~> 2.0"}
     ]
   end
 
